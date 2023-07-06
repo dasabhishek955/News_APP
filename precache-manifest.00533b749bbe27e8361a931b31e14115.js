bd4d26a9560d49011f2d55f128939cf31e344bd9
Abhishek Das <dasabhishek955@gmail.com>
2023-07-06 17:56:59 +0530

    Updates

---
+++ precache-manifest.00533b749bbe27e8361a931b31e14115.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb08a946b72923c62dd9",
+    "url": "/News_APP/static/css/main.caaf87ea.chunk.css"
+  },
+  {
+    "revision": "cb08a946b72923c62dd9",
+    "url": "/News_APP/static/js/main.cb08a946.chunk.js"
+  },
+  {
+    "revision": "9594a6bb494a9d9ba425",
+    "url": "/News_APP/static/js/1.9594a6bb.chunk.js"
+  },
+  {
+    "revision": "6217e1404f13b40fbe8b",
+    "url": "/News_APP/static/js/2.6217e140.chunk.js"
+  },
+  {
+    "revision": "35781fa935513052826c",
+    "url": "/News_APP/static/js/runtime~main.35781fa9.js"
+  },
+  {
+    "revision": "2bb5174946581360f763a6eb3154056e",
+    "url": "/News_APP/index.html"
+  }
+];
